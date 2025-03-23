92e6e8e71c6f0a6829cdab94ed4ba96f95e12ada
Mathew Bravo <mathewbravo94@gmail.com>
2025-03-23 00:43:06 -0400

    questioning my sanity, need to plan this better

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,6 +1,23 @@
+use std::path::Path;
+
 #[tauri::command]
 async fn open_file(file_path: String){
     println!("Recieved {}", file_path);
+
+    let path = Path::new(&file_path);
+    
+    match path.extension().and_then(|ext| ext.to_str()){
+        Some("csv") => {
+            println!("Got a csv")
+        }   
+        None => {
+            println!("Failed to find a file extension")
+        }    
+        _ => {
+            println!("Not a CSV")
+        }
+    }
+
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
